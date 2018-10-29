fbb6356259b703e054e9a3b90163f69e4c717264
Filip Pieronkiewicz <987654321fp@gmail.com>
2018-10-29 18:26:16 +0100

    few final changes, fixing ex 3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,112 +1,128 @@
 package com.company;
-import java.util.Scanner;
+
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) {
-//	    // EX.1
-//      //zadanie: zsumować wszystkie liczby całkowite od 1 do 1000000
-//        int i ;
-//        long result = 0;
-//
-//        for(i = 1; i<100000;i++){
-//            result +=i;
-//        }
-//        System.out.println(result);
+        Scanner in = new Scanner(System.in);
+        int switcher;
 
-//        //EX.2
-//        //zadanie2: dane liczby całkowite (int) a, b, c;
-//        // --> sprawdzić czy da się ułożyć trójkąt z bokami o długości a,b,c
+        System.out.println("Chose which exercise to run: 1/2/3/4/5");
+        switcher = in.nextInt();
+        switch (switcher) {
+            case 1:
+                ex1();
+                break;
+            case 2:
+                ex2();
+                break;
+            case 3:
+                ex3();
+                break;
+            case 4:
+                ex4();
+                break;
+            case 5:
+                ex5();
+                break;
+        }
+    }
 
+    // EX.1
+    private static void ex1() {
+        long result = 0;
 
-//        Scanner reader = new Scanner(System.in);  // Reading from System.in
-//        int a,b,c;
-//        System.out.println("Input please variables to check if You can make triangle out of them (a,b,c)");
-//            a = reader.nextInt();
-//            b = reader.nextInt();
-//            c = reader.nextInt();
-//
-//
-//        reader.close();
-//
-//        if(a+b>c && a+c>b && b+c>a){
-//            System.out.println("That variables can form triangle");
-//        }else{
-//
-//            System.out.println("this variables can't for a triangle");
-//        }
-        //EX.3
-        //zadanie3: z liczb z przedziału [1..100] znaleźć takie a,b,c:
-        //  a*a + b*b == c*c
+        System.out.println("EX.1: Add up all the integer numbers from 1 to 1000000");
+        for (int i = 1; i <= 100000; i++) {
+            result += i;
+        }
+        System.out.printf("Result: %d%n", result);
+    }
 
+    //EX.2
+    private static void ex2() {
+        Scanner in = new Scanner(System.in);
+        int a, b, c;
 
-        int solution = 0;
-        int d, e;
+        System.out.println("EX.2: Check if given segments can form a triangle");
+        System.out.println("Please input variables, to check if You can make triangle out of them (a,b,c)");
+        a = in.nextInt();
+        b = in.nextInt();
+        c = in.nextInt();
+        in.close();
+        if (a + b > c && a + c > b && b + c > a) {
+            System.out.println("That variables can form a triangle");
+        } else {
+            System.out.println("That variables can't form a triangle");
+        }
+    }
 
+    //EX.3
+    private static void ex3() {
+        int result = 0;
+        double product;
 
-        for(d = 1 ; d<101; d++){
-            for(e=1;e<101;e++){
-                if( Math.sqrt(e*e+d*d) <= 100 && Math.sqrt(e*e+d*d)>=1 && Math.sqrt(e*e+d*d) == (int) Math.sqrt(e*e+d*d)){
-                    solution +=1;
+        System.out.println("EX.3 Find such numbers, in range 1:100, that fulfills such condition: a^2+b^2=c^2");
+        for (int a = 1; a <= 100; a++) {
+            for (int b = 1; b <= 100; b++) {
+                product = Math.sqrt(b * b + a * a);
+                if (product <= 100 && product >= 1 && product == (int) product) {
+                    result++;
                 }
             }
-
         }
-        System.out.println(solution);
+        System.out.printf("There are : %d such numbers%n", result);
+    }
 
-        //EX.4
-        //zadanie4: obecny czas zadany jest w postaci dwóch liczb całkowitych,
-        // hh (godzina) i mm (minuta); np. int hh=17; int mm=15 odpowiadają 17:15;
-        // podany jest tez odcinek czasu `int delta`; jest to liczba minut,
-        // które chcemy dodać do obecnego czasu; proszę wyliczyć
-        // godzinę i minutę chwili powstałej po dodaniu `delta` do obecnego czasu.
-//
-//        int h, m, delta, time;
-//        String minutes;
-//
-//        Scanner reader = new Scanner(System.in);  // Reading from System.in
-//        System.out.println("Input numbers as follows : h, m, delta");
-//            h = reader.nextInt();
-//            m = reader.nextInt();
-//            delta = reader.nextInt();
-//
-//
-//        reader.close();
-//        time = h*60+m+delta;
-//
-//        if(time%60<=9){
-//            minutes = "0" + time%60;
-//        }else{
-//            minutes = Integer.toString(time%60);
-//        }
-//
-//        System.out.println("It is: " + h + ":" + m + " in " + delta + "minutes it will be " +  time/60 + ":" + minutes);
+    //EX.4
+    private static void ex4() {
+        int hours, minutes, minutesIncrement, timeCalculated;
+        String minutesFormatted;
+        Scanner in = new Scanner(System.in);
 
-        //EX.5
-        //chinczyk
-//        Random generator = new Random();
-//
-//
-//        int a = 1;
-//        int b = 1;
-//        while(a < 1000 && b < 1000){
-//            a += generator.nextInt(6)+1;
-//            if(a==b){
-//                b=1;
-//            }
-//            b += generator.nextInt(6)+1;
-//            if(a==b){
-//                a=1;
-//            }
-//        }
-//
-//        if(a>=1000){
-//            System.out.println("A won");
-//        }else{
-//            System.out.println("B won");
-//        }
+        System.out.println("EX.4: Create time calculator, to add up given minutes to given current time");
+        System.out.println("Input numbers as follows : hour, minutes, increment in minutes");
+        hours = in.nextInt();
+        minutes = in.nextInt();
+        minutesIncrement = in.nextInt();
+        in.close();
+        timeCalculated = hours * 60 + minutes + minutesIncrement;
 
+        if (timeCalculated % 60 <= 9) {
+            minutesFormatted = "0" + timeCalculated % 60;
+        } else {
+            minutesFormatted = Integer.toString(timeCalculated % 60);
+        }
+        if(timeCalculated / 60 >= 25){
+            timeCalculated -= 24*60;
+        }
+        System.out.printf("It is: %d:%d, in %d minutes it will be %d:%s%n", hours, minutes, minutesIncrement, timeCalculated / 60, minutesFormatted);
+    }
+
+    //EX.5 aka "Chinczyk"
+    private static void ex5() {
+        Random generator = new Random();
+        int playerAPosition = 1;
+        int playerBPosition = 1;
+
+        System.out.println("EX.5: write simple board game simulator aka Ludo");
+        while (playerAPosition < 1000 && playerBPosition < 1000) {
+            playerAPosition += generator.nextInt(6) + 1;
+            if (playerBPosition == playerAPosition) {
+                playerBPosition = 1;
+            }
+            playerBPosition += generator.nextInt(6) + 1;
+            if (playerAPosition == playerBPosition) {
+                playerAPosition = 1;
+            }
+        }
+        if (playerAPosition >= 1000) {
+            System.out.println("A won");
+        } else {
+            System.out.println("B won");
+        }
     }
 }
